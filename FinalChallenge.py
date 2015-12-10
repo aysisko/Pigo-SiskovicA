2cb3a976c78e715df9dae17c7f32211749a15a02
unknown <aysiskovic@gmail.com>
2015-12-10 08:26:20 -0500

    Test run

--- FinalChallenge.py
+++ FinalChallenge.py
@@ -40,70 +40,108 @@ class Pigo:
                 bwd()
             print "Back it up"
 
+    def rightrot(self):
+            self.status["isMoving"] = True
+            for x in range(3):
+                right_rot()
+            print "rotating right"
+
     ######
     ##COMPLEX METHODS
     ######
     def servoSweep(self):
+        print "Checking for obstacles!"
         for ang in range(20, 160, 2):
             servo(ang)
             time.sleep(.1)
-            vision[ang] = us_dist(15)
-            print "Checking for obstacles!"
+            self.vision[ang] = us_dist(15)
 
-    def turnTo(self):
-        if ang < 30 and ang > 0:
-            right_rot()
-            time.sleep(1)
-            self.stop()
-        if ang < 60 and ang > 31:
-            right_rot()
-            time.sleep(.5)
-            self.stop()
-        if ang < 90 and ang > 61:
-            right_rot()
-            time.sleep(.2)
-            self.stop()
-        if ang > 90 and  ang < 120:
-            left_rot()
-            time.sleep(.2)
-            self.stop()
-        if ang > 121 and ang < 150:
-            left_rot()
-            time.sleep(.5)
-            self.stop()
-        if ang > 151 and ang < 180:
-            left_rot()
-            time.sleep(.1)
-            self.stop()
-        else:
-            print "Uhhhhh this isn't working."
+    def turnAround(self):
+        self.rightrot()
+        time.sleep(1.5)
+        self.stop()
 
+    #returns true if there's any 20 degree slice
     def checkSpan(self):
         counter = 0
         for ang in range(20, 160, 2):
-            if vision[ang] > SAFE_DISTANCE:
+            if self.vision[ang] > self.MIN_DIST:
                 counter += 1
                 print "Looking for a clear path."
             else:
                 counter = 0
                 print "Looking for a clear path."
-            if counter == 20:
-                print "Let's Go!"
-                self.turnTo()
-                return ang
+            if counter >= 10:
+                print "checkSpan found at least one option available"
+                return True
+        print "checkSpan says no options exist"
+        return False
+
+    def turnTo(self):
+        ang = 90
+        answer = None
+        counter = 0
+        option = [0] * 20
+        optindex = 0
+        for ang in range(20, 160, 2):
+            if self.vision[ang] > self.MIN_DIST:
+                counter += 1
+                print "Looking for a clear path."
+            else:
+                counter = 0
+                print "Looking for a clear path."
+            if counter >= 10:
+                option[optindex] = ang - 10
+                answer = option[optindex]
+                optindex += 1
+                counter = 0
+        if answer:
+            ang = answer
+            print ang
+            print answer
+            if ang < 30 and ang > 0:
+                right_rot()
+                time.sleep(1)
+                self.stop()
+            if ang < 60 and ang > 31:
+                right_rot()
+                time.sleep(.5)
+                self.stop()
+            if ang < 90 and ang > 61:
+                right_rot()
+                time.sleep(.2)
+                self.stop()
+            if ang > 90 and  ang < 120:
+                left_rot()
+                time.sleep(.2)
+                self.stop()
+            if ang > 121 and ang < 150:
+                left_rot()
+                time.sleep(.5)
+                self.stop()
+            if ang > 151 and ang < 180:
+                left_rot()
+                time.sleep(.1)
+                self.stop()
+        else:
+            print "Uhhhhh this isn't working."
 
     def checkDist(self):
+        servo(90)
         self.status['dist'] = us_dist(15)
         print "I see something" + str(self.status['dist']) + "mm away."
+        if self.status['dist'] < self.MIN_DIST:
+            return False
+        else:
+            return True
 
     def safeDrive(self):
         self.fwd()
-        while self.fwd():
-            self.checkDist()
-            if 'dist' < 10:
-                self.stop()
+        while self.checkDist():
+            time.sleep(.05)
+        self.stop()
 
-    def findPathRight(self):
+    def avoider(self):
         self.safeDrive()
         print "Running safeDrive."
         if self.status['dist'] < self.MIN_DIST:
@@ -118,5 +156,14 @@ class Pigo:
 
 #Main APP starts here
 tina = Pigo()
-tina.findPathRight()
+
+while True:
+    if tina.checkDist():
+        tina.safeDrive()
+    else:
+        tina.servoSweep()
+        if tina.checkSpan():
+            tina.turnTo()
+        else:
+            tina.turnAround()
 
